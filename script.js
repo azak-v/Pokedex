e0431940767c4bfa6bbd545113873a1d1afd86c5
Victor Principe <116037521+azak-v@users.noreply.github.com>
2023-04-25 20:09:25 -0300

    started the js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function script(){
-    
+    let index = 1;
+    let link = "https://pokeapi.co/api/v2/pokemon/"+index;
+    let pokemonName = document.getElementById("pokemon-name");
+    let pokemonNumber = document.getElementById("pokemon-number");
+    let leftArrow = document.getElementById("left-arr")
+    let rightArrow = document.getElementById("right-arr")
+
+    rightArrow.addEventListener('click', ()=>{
+        index++;
+        
+
+    })
+
+    function doFetch(arrow){}
+    //query selectors do span do nome
+    //Event listeners
+    //função para fazer o fetch
+    //fazer o fetch somente se a variavel não for 0
+    //adicione mais um a uma variavel e concatene ao link
+    //faça o fetch
+    //armazene o nome do filho da puta
+    // fetch(link)
+    //     .then(response => response.json())
+    //     .then(pokemon => console.log(pokemon.name));
+
+
 }//Dom
