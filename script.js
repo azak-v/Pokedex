786bc902aa2a241a615e405393376e278ccbcd73
azak-ana <victorprincipe1@hotmail.com>
2023-04-26 16:50:12 -0300

    almost done the  js

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+const { response } = require("express");
+
 function script(){
     let index = 1;
-    let link = "https://pokeapi.co/api/v2/pokemon/"+index;
+    let link = "https://pokeapi.co/api/v2/pokemon/1"+index;
     let pokemonName = document.getElementById("pokemon-name");
     let pokemonNumber = document.getElementById("pokemon-number");
+    let pokemonImg = document.getElementById("pokemon-img");
     let leftArrow = document.getElementById("left-arr")
     let rightArrow = document.getElementById("right-arr")
 
-    rightArrow.addEventListener('click', ()=>{
+    rightArrow.addEventListener('click', async ()=>{
         index++;
-        
+        link = "https://pokeapi.co/api/v2/pokemon/"+index;
+        console.log(link);
+        await fetch(link)
+            .then(response => response.json())
+            .then(pokemon => {
+                console.log(pokemon);
+                pokemonName.innerText = pokemon.name;
+                pokemonImg.src = pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default
+                pokemonNumber.innerText = index});
+    })
 
+    leftArrow.addEventListener('click', async ()=>{
+        if(index!=1){
+            index--;
+        }
+        link = "https://pokeapi.co/api/v2/pokemon/"+index;
+        console.log(link);
+        await fetch(link)
+            .then(response => response.json())
+            .then(pokemon => {
+                pokemonName.innerText = pokemon.name
+                pokemonNumber.innerText = index
+                });
     })
 
     function doFetch(arrow){}
